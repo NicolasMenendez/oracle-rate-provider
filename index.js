f20266abe50d45caccf4c84e07db3d6e4045591f
rotcivegaf <victorfage@gmail.com>
2019-08-09 14:52:05 -0300

    add express

--- index.js
+++ index.js
@@ -42,3 +42,13 @@ async function main(){
 }
 
 main();
+
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.send(console.log('Node js server is running')))
+  .listen(PORT, () => console.info(`Listening on ${ PORT }`));
