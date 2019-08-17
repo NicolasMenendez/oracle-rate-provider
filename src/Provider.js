d6c0e4afa38a9312027750725a41fa949536bf28
Nicolas <menendeznm@gmail.com>
2019-08-17 13:42:47 -0300

    add more markets

--- src/Provider.js
+++ src/Provider.js
@@ -1,36 +1,54 @@
-const Markets = require('./Markets.js');
+const MarketsManager = require('./MarketsManager.js');
 
 module.exports = class Provider {
   constructor(w3, oracleFactory, oracles) {
     this.w3 = w3;
     this.oracleFactory = oracleFactory;
     this.oracles = oracles;
-    this.markets = new Markets(w3);
+    this.MarketsManager = null;
+  }
+
+  async init() {
+    this.MarketsManager = await new MarketsManager(this.w3).init();
+    return this;
   }
 
   async provideRates(signer) {
     for (let data of signer.data) {
+
       if(!this.oracles[data.currency]){
         console.log('Wrong currency: ' + data.currency);
         continue;
       }
 
-      const rate = await this.markets.getRate(data);
+      const rate = await this.MarketsManager.getRate(data);
       if(!rate){
-        console.log('Wrong rate: ' + rate.toString());
+        console.log('Wrong rate: ' + rate);
         continue;
       }
 
+      const address = this.oracles[data.currency]._address;
+      if (!address) {
+        console.log('Wrong address: ' + address);
+        continue;
+      }
+
+      console.log('Starting send transaction with marmo');
+
       const gasPrice = await this.w3.eth.getGasPrice();
-      const gasEstimate = await this.oracleFactory.methods.provide(this.oracles[data.currency]._address, rate.toString()).estimateGas(
+      //console.log('GasPrice: ', gasPrice);
+      const gasEstimate = await this.oracleFactory.methods.provide(address, rate).estimateGas(
         { from: signer.address }
       );
-
+      // console.log('GasEstimate: ', gasEstimate);
+      // 10% more than gas estimate
+      const moreGasEstimate = (gasEstimate * 1.5).toFixed(0);
+      // console.log('MoreGasEstimate: ', moreGasEstimate);
       const log = 'Provide(signer: ' + signer.address + ', rate: ' + rate + ')';
 
       try {
-        const tx = await this.oracleFactory.methods.provide(this.oracles[data.currency]._address, rate).send(
-          { from: signer.address, gas: gasEstimate, gasPrice: gasPrice }
+        const tx = await this.oracleFactory.methods.provide(address, rate).send(
+          { from: signer.address, gas: moreGasEstimate, gasPrice: gasPrice }
         );
         console.log(log + ', txHash: ' + tx.transactionHash);
       } catch(e) {
